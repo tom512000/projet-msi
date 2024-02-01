eb2d778bba5579c7008cdc272b5cf04bc4b1b972
ratt0008 <louis.rattanavong@etudiant.univ-reims.fr>
2024-02-01 16:29:48 +0100

    Initialisation du formulaire

--- public/index.php
+++ public/index.php
@@ -6,12 +6,26 @@ require_once('../autoload.php');
 
 $webPage = new Webpage();
 
-$webPage->setTitle('ll');
+$webPage->setTitle('Calcul d\'intérêts');
 
 $webPage->appendContent(
     <<<HTML
-        Pd
+    <div>
+        <form action="resultForm.php" method="post">
+            <label for="valeurDep">Valeur de départ :</label>
+            <input type="number" id="valeurDep" name="valeurDep" required><br><br>
+            
+            <label for="tauxInteret">Taux d'intérêt (%):</label>
+            <input type="number" id="tauxInteret" name="tauxInteret" required><br><br>
+            
+            <label for="duree">Durée (en années) :</label>
+            <input type="number" id="duree" name="duree" required><br><br>
+            
+            <button type="submit">Calculer</button>
+        </form>
+    </div>
 HTML
 );
 
 echo $webPage->toHTML();
+?>
